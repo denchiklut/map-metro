4dac1657f88497f60a2f189662c8828dd4ed4294
daaleksandrov <daaleksandrov@domclick.ru>
2020-06-24 22:03:51 +0300

    Updates

---
+++ precache-manifest.657b24ce41b6b557a20a26508c44c6b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a125b517e94170540cd46090598a6b",
+    "url": "/map-metro/index.html"
+  },
+  {
+    "revision": "c870161b4b63928311e1",
+    "url": "/map-metro/static/css/main.c7097a59.chunk.css"
+  },
+  {
+    "revision": "386402ff231afeb306ed",
+    "url": "/map-metro/static/js/2.ed7ca831.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/map-metro/static/js/2.ed7ca831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c870161b4b63928311e1",
+    "url": "/map-metro/static/js/main.0aeef574.chunk.js"
+  },
+  {
+    "revision": "e5fad2df3f0770b60a0e",
+    "url": "/map-metro/static/js/runtime-main.1d9e3802.js"
+  }
+]);

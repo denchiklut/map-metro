692b807a81892820205e6768791019833e5c00f2
daaleksandrov <daaleksandrov@domclick.ru>
2020-06-25 16:05:48 +0300

    fix moves

--- src/components/Metro/index.tsx
+++ src/components/Metro/index.tsx
@@ -1,5 +1,5 @@
 import React, { Component, RefObject } from 'react'
-import panzoom from 'panzoom'
+import panzoom, { PanZoom } from 'panzoom'
 
 import { schemeData } from '../../data/moscow/schemeData'
 import { schemeMeta } from '../../data/moscow/schemeMeta'
@@ -8,29 +8,62 @@ import { resources } from '../../data/moscow/resources'
 import MetroScheme from '../MetroScheme'
 
 import styles from './Metro.module.css'
+import { moove } from '../../helpers'
 
 class Metro extends Component {
     parentRef: RefObject<HTMLDivElement> = React.createRef()
+    schemeRef: RefObject<HTMLDivElement> = React.createRef()
 
     componentDidMount() {
-        if (this.parentRef.current) {
-            panzoom(this.parentRef.current, {
+        if (this.schemeRef.current) {
+            const instance: PanZoom = panzoom(this.schemeRef.current, {
                 zoomSpeed: .1,
                 pinchSpeed: 2,
                 smoothScroll: true,
                 maxZoom: schemeMeta.scale.max / schemeData.assetZoom,
                 minZoom: schemeMeta.scale.min / schemeData.assetZoom
             })
+
+            this.setInitialSchemeState(instance)
+            this.setSchemeEvents(instance)
         }
+    }
+
+    setInitialSchemeState = (panzoom: PanZoom) => {
+        if (this.schemeRef.current) {
+            const rect = this.schemeRef.current.getBoundingClientRect()
+            const width = rect.width / 2
+            const height = rect.height / 2
+
+            const x = width - schemeMeta.center.x
+            const y = height - schemeMeta.center.y
+
+            panzoom.moveBy(x, y, false)
+            panzoom.zoomTo(0, -height / 2,schemeMeta.scale.initial / schemeData.assetZoom)
+        }
+    }
+
+    setSchemeEvents = (panzoom: PanZoom) => {
+        panzoom.on('pan', () => {
+            if (this.parentRef.current && this.schemeRef.current) {
+                moove(panzoom, this.parentRef.current, this.schemeRef.current)
+            }
+        })
+
+        panzoom.on('zoom', () => {
+            if (this.parentRef.current && this.schemeRef.current) {
+                moove(panzoom, this.parentRef.current, this.schemeRef.current)
+            }
+        })
     }
 
     render() {
         return (
-            <div className={ styles.schemeContainer }>
+            <div className={ styles.schemeContainer } ref={ this.parentRef }>
                 <div className={ styles.schemeView }>
                     <div
                         className={ styles.app }
-                        ref={ this.parentRef }
+                        ref={ this.schemeRef }
                     >
                         <MetroScheme
                             size={ schemeMeta.size }
--- src/helpers.tsx
+++ src/helpers.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { PanZoom } from "panzoom"
 import { Circle, Path, Shapes, Text } from './data/moscow/schemeData'
 
 export const renderLines = (items: (Path|Circle|Text)[]): (JSX.Element | null)[]=> {
@@ -10,4 +11,31 @@ export const renderLines = (items: (Path|Circle|Text)[]): (JSX.Element | null)[]
             default: return null;
         }
     })
+}
+
+export function moove(panzoom: PanZoom, parentRef: HTMLDivElement, schemeRef: HTMLDivElement): void {
+    if (parentRef && schemeRef) {
+        const offset = 112
+        const transform = panzoom.getTransform()
+        const width = schemeRef.offsetWidth * transform.scale
+        const height = schemeRef.offsetHeight * transform.scale
+        const isWider = width > parentRef.offsetWidth
+        const isHigher = height > parentRef.offsetHeight - offset
+        const parentY = isHigher ? parentRef.offsetHeight / 2 : parentRef.offsetHeight - height / 2
+        const schemeY = isHigher ? parentRef.offsetHeight / 2 + offset - height : -height / 2 + offset
+        const parentX = isWider ? parentRef.offsetWidth / 2 : parentRef.offsetWidth - width / 2
+        const schemeX = isWider ? parentRef.offsetWidth / 2 - width : -width / 2
+
+        if (transform.x > parentX ) {
+            panzoom.moveTo(parentX, transform.y)
+        } else if (transform.x < schemeX) {
+            panzoom.moveTo(schemeX, transform.y)
+        }
+
+        if (transform.y > parentY ) {
+            panzoom.moveTo(transform.x, parentY)
+        } else if (transform.y < schemeY) {
+            panzoom.moveTo(transform.x, schemeY)
+        }
+    }
 }

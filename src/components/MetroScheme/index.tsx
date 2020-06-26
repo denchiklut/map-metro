fd0b49a17c91eb8af1fc733ef1d9a4f24848d2bf
daaleksandrov <daaleksandrov@domclick.ru>
2020-06-26 22:15:40 +0300

    Select

--- src/components/MetroScheme/index.tsx
+++ src/components/MetroScheme/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Circle, Path, Text } from '../../data/moscow/schemeData'
 
 import { renderLines } from '../../helpers'
@@ -11,42 +11,55 @@ import styles from './MetroScheme.module.css'
 interface IProps {
     size: { width: number, height: number }
     resources: Path[]
-    lines: (Path|Circle)[]
-    lineLabels: (Circle|Text)[]
+    lines: (Path | Circle)[]
+    lineLabels: (Circle | Text)[]
     transfers: Path[]
     stations: Circle[]
     labels: Text[]
 }
 
-const MetroScheme = (props: IProps) => {
-    const {
-        size,
-        resources,
-        lines,
-        lineLabels,
-        transfers,
-        stations,
-        labels
-    } = props
+class MetroScheme extends Component<IProps> {
+    onStationClick = (stationId: string) => {
+        let station: Text|null = null;
+        this.props.labels.forEach((label: Text) => {
+            if (label.id === stationId) {
+                station = label
+            }
+        })
+        console.log(station)
+    }
 
-    return (
-        <div>
-            <svg
-                version='1.1'
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox={ `0 0 ${ size.width } ${ size.height }` }
-                className={ styles.mapSvg }
-            >
-                { renderLines(resources) }
-                { renderLines(lines) }
-                { renderLines(lineLabels) }
-                { renderLines(transfers) }
 
-                <MetroStation stations={ stations } />
-                <MetroLabel labels={ labels } />
-            </svg>
-        </div>
-    )
+    render() {
+        const {
+            size,
+            resources,
+            lines,
+            lineLabels,
+            transfers,
+            stations,
+            labels
+        } = this.props
+
+        return (
+            <div>
+                <svg
+                    version='1.1'
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox={`0 0 ${size.width} ${size.height}`}
+                    className={styles.mapSvg}
+                >
+                    {renderLines(resources)}
+                    {renderLines(lines)}
+                    {renderLines(lineLabels)}
+                    {renderLines(transfers)}
+
+                    <MetroStation stations={stations} onStationClick={this.onStationClick}/>
+                    <MetroLabel labels={labels}/>
+                </svg>
+            </div>
+        )
+    }
 }
 
 export default MetroScheme
--- src/components/MetroStation/index.tsx
+++ src/components/MetroStation/index.tsx
@@ -6,21 +6,37 @@ import styles from './MetroStation.module.css'
 
 interface IProps {
     stations: Circle[]
+    onStationClick: (stationId: string) => void
 }
 
 class MetroStation extends Component<IProps> {
+    onStationClick = (station: Circle) => () => {
+        if (station.stationId) {
+            this.props.onStationClick(station.stationId)
+        }
+    }
+
     render() {
         const { stations } = this.props
 
         return stations.map((station: Circle, index: number):JSX.Element => (
-            <g key={ index } className={ styles.station }>
+            <g
+                key={ index }
+                className={ styles.station }
+                onClick={ this.onStationClick(station) }
+            >
                 <circle
                     fill='white'
                     r={ 8 }
                     cx={ station.cx }
                     cy={ station.cy }
                 />
-                <circle { ...station } />
+                <circle
+                    fill={ station.fill }
+                    r={ station.r }
+                    cx={ station.cx }
+                    cy={ station.cy }
+                />
             </g>
         ))
     }
